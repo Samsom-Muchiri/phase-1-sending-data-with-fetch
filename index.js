f450502fa3a03fbb3f2d0c916c3ad9194b81eee5
Sam .M <you@example.com>
2023-06-26 18:32:13 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('userDataForm');
+  const nameInput = document.getElementById('nameInput');
+  const emailInput = document.getElementById('emailInput');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    submitData(name, email)
+      .then(data => {
+        const userId = data.id;
+        appendUserId(userId);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        displayErrorMessage(errorMessage);
+      });
+  });
+});
+
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch('http://localhost:3000/users', requestOptions)
+    .then(response => response.json());
+}
+
+function appendUserId(userId) {
+  const userIdElement = document.createElement('p');
+  userIdElement.textContent = `User ID: ${userId}`;
+  document.body.appendChild(userIdElement);
+}
+
+function displayErrorMessage(message) {
+  const errorElement = document.createElement('p');
+  errorElement.textContent = `Error: ${message}`;
+  document.body.appendChild(errorElement);
+}
